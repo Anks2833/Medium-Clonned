ad8d382481a0dc03471e066522fddd57928cf575
Anks <dubeyankur2833@gmail.com>
2023-12-25 15:07:34 +0530

    first one

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("scroll", function(){
+    const header = document.querySelector(".navi");
+
+    if(window.scrollY > 480){
+        header.classList.add("scrolled");
+    }
+
+    else {
+        header.classList.remove("scrolled");
+    }
+});
+
